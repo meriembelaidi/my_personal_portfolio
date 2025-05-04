58b375f97e3dc56e09f870eb35c7a11ac37817fb
meriembelaidi <162323537+meriembelaidi@users.noreply.github.com>
2025-05-04 11:50:35 +0100

    Initial commit

---
+++ components/Skills.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Skills() {
+  return (
+    <section id="skills">
+      <h2>Skills</h2>
+      <ul>
+        <li>Java, C, C++</li>
+        <li>HTML, CSS, JavaScript, React</li>
+        <li>SQL, Git, Linux</li>
+      </ul>
+    </section>
+  );
+}
+
+export default Skills;
